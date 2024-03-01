795a996ae16bcd4062781c7dc00138b8f35b8abf
Stanislav Tsvietkov <s.tsvyetkov@gmail.com>
2024-03-01 11:39:00 +0200

    process_sales pipeline

---
+++ lesson_07/airflow/dags/final_process_sales.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from datetime import datetime
+
+
+with DAG(
+    dag_id="final_process_sales",
+    start_date=datetime(2022, 9, 1),
+    end_date=datetime(2022, 10, 1),
+    schedule_interval='0 2 * * *',
+    max_active_runs=1,
+    catchup=True
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    transfer_to_bq = GCSToBigQueryOperator(
+        task_id='transfer_to_bq',
+        bucket='raw_data_final_project',
+        source_objects=f"sales/{{{{macros.datetime.strptime( ds , '%Y-%m-%d').strftime('%Y-%m-%-d')}}}}"
+                       f"/{{{{macros.datetime.strptime( ds , '%Y-%m-%d').strftime('%Y-%m-%-d')}}}}__sales.csv",
+        destination_project_dataset_table='de-07-stas-tsvietkov.bronze.sales_raw',
+        schema_fields=[
+            {'name': 'CustomerId', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'PurchaseDate', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Product', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Price', 'type': 'STRING', 'mode': 'NULLABLE'}
+        ],
+        skip_leading_rows=1,
+        write_disposition='WRITE_APPEND',
+        gcp_conn_id='gc-de'
+    )
+
+    end = DummyOperator(task_id='end')
+
+    start >> transfer_to_bq >> end
