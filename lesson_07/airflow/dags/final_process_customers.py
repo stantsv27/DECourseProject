25761c0211c555c1e361994066bacb0ece1735b7
Stanislav Tsvietkov <s.tsvyetkov@gmail.com>
2024-03-01 15:44:14 +0200

    process_customers pipeline added

---
+++ lesson_07/airflow/dags/final_process_customers.py
@@ -0,0 +1,57 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+
+with DAG(
+    dag_id="final_process_customers",
+    start_date=datetime(2022, 8, 1),
+    end_date=datetime(2022, 8, 6),
+    schedule_interval='0 2 * * *',
+    max_active_runs=1,
+    catchup=True
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    transfer_to_bq = GCSToBigQueryOperator(
+        task_id='transfer_to_bq',
+        bucket='raw_data_final_project',
+        source_objects=f"customers/{{{{macros.datetime.strptime( ds , '%Y-%m-%d').strftime('%Y-%m-%-d')}}}}/*.csv",
+        destination_project_dataset_table='de-07-stas-tsvietkov.bronze.customers_raw',
+        schema_fields=[
+            {'name': 'Id', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'FirstName', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'LastName', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'Email', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'RegistrationDate', 'type': 'STRING', 'mode': 'NULLABLE'},
+            {'name': 'State', 'type': 'STRING', 'mode': 'NULLABLE'}
+        ],
+        skip_leading_rows=1,
+        write_disposition='WRITE_TRUNCATE',
+        gcp_conn_id='gc-de'
+    )
+
+    transfer_to_silver = BigQueryInsertJobOperator(
+        task_id='transfer_to_silver',
+        dag=dag,
+        configuration={
+            "query": {
+                "query": "{% include 'sql/customers_to_silver.sql' %}",
+                "useLegacySql": False,
+                "writeDisposition": "WRITE_TRUNCATE",
+                "destinationTable": {
+                    "projectId": 'de-07-stas-tsvietkov',
+                    "datasetId": 'silver',
+                    "tableId": 'customers'
+                }
+            }
+        },
+        gcp_conn_id='gc-de'
+    )
+
+    end = DummyOperator(task_id='end')
+
+    start >> transfer_to_bq >> transfer_to_silver >> end
